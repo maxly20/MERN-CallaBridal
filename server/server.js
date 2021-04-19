bab7a230ff376327f4ab48c3750fd993d043119b
maxly20 <maxly2011@gmail.com>
2021-04-19 16:05:25 +1000

    building server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
