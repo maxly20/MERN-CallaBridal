9f633252627de96104b73e4270cdcbf239b7ef8d
maxly20 <maxly2011@gmail.com>
2021-05-07 20:40:20 +1000

    building header

--- client/src/components/navigation/header.js
+++ client/src/components/navigation/header.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
-const header = () => {
-    return (
-        <div>
-            
+const Header = () => {
+  return (
+    <header className='bck_b_light'>
+      <div className='container'>
+        <div className='left'>
+          <div className='logo'>CALLA BRIDAL</div>
         </div>
-    )
-}
+        <div className='right'>
+          <div className='top'>
+            <>
+              <div className='cart_link'>
+                <span>1</span>
+                <Link to='/dashboard/user/user_cart'>My cart</Link>
+              </div>
 
-export default header
+              <Link to='/dashboard'>My account</Link>
+              <span onClick={() => alert('log out')}>Log out</span>
+
+              <Link to='/sign_in'>Log in</Link>
+            </>
+          </div>
+          <div className='bottom'>
+            <Link to='/'>Home</Link>
+            <Link to='/shop'>Shop</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
