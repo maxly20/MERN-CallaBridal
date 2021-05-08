3a31012d555d7c3ce3087fa26f695137c027cfef
maxly20 <maxly2011@gmail.com>
2021-05-08 21:01:06 +1000

    building slider

--- client/src/components/home/featured.js
+++ client/src/components/home/featured.js
@@ -1,6 +1,35 @@
+import Carrousel from 'utils/carrousel';
 
-const featured = () => {
-  return <div>featured</div>;
+const Featured = () => {
+  const corrouselItems = [
+    {
+      img: '/images/featured/featured_home.jpg',
+      lineOne: 'CALLA LIMITED 2021',
+      lineTwo: 'Custom Designs',
+      lineTitle: 'Show Now',
+      linkTo: '/shop',
+    },
+    {
+      img: '/images/featured/featured_home_2.jpg',
+      lineOne: 'CALLA COUTURE 2021',
+      lineTwo: 'Custom Designs',
+      lineTitle: 'View Catalogue',
+      linkTo: '/shop',
+    },
+    {
+      img: '/images/featured/featured_home_3.jpg',
+      lineOne: 'CALLA COLLECTIONS 2021',
+      lineTwo: 'Custom Designs',
+      lineTitle: 'View Collections',
+      linkTo: '/shop',
+    },
+  ];
+
+  return (
+    <div className='featured_container'>
+      <Carrousel items={corrouselItems} />
+    </div>
+  );
 };
 
-export default featured;
+export default Featured;
